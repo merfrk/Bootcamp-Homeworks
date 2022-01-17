964e03b60d7a6c5c7a2796f48d3939e3a4c9ad5c
merfrk <87045574+merfrk@users.noreply.github.com>
2022-01-18 00:17:30 +0300

    Add files via upload

---
+++ bootcampWeek1/src/main/java/com/example/bootcampweek1/BootcampWeek1Application.java
@@ -0,0 +1,14 @@
+package com.example.bootcampweek1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampWeek1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootcampWeek1Application.class, args);
+        System.out.println("Hello");
+    }
+
+}
---
+++ bootcampWeek1/src/test/java/com/example/bootcampweek1/BootcampWeek1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootcampweek1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampWeek1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
